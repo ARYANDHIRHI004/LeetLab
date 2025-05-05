dcfce37a7ed39bc59547d53f077cf4d38cb168e3
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-05-05 09:49:08 +0530

    feat: update problem controller created

--- Backend/src/controllers/problem.controllers.js
+++ Backend/src/controllers/problem.controllers.js
@@ -140,6 +140,92 @@ export const updateProblem = async (req, res) => {
   //id
   //check
   //same as create
+  const {id} = req.params;
+  
+  if(req.user.role !== "ADMIN"){
+    return res.status(401).json({
+        message:"You are not authorized"
+    })
+  }
+
+  const {title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    refrenceSolution } = req.body
+
+  try {
+
+    for (const [language, solutionCode] of Object.entries(refrenceSolution)) {
+      const languageId = getJudge0LanguageId(language);
+        
+      if (!languageId) {
+        return res
+          .status(400)
+          .json({ error: `Language ${language} is not supported` });
+      }
+
+      //
+      const submissions = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
+
+      const submissionResults = await submitBatch(submissions);
+
+      const tokens = submissionResults.map((res) => res.token);
+
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        console.log("Result-----" , result)
+        // console.log(
+        //   `Testcase ${i + 1} and Language ${language} ----- result ${JSON.stringify(result.status.description)}`
+        // );
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `Testcase ${i + 1} failed for language ${language}`,
+          });
+        }
+      }
+    }
+
+    const updatedProblem = await db.problem.update({
+      where:{
+        id
+      },
+      data:{
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        refrenceSolution
+      }
+    })
+
+    return res.status(201).json({
+      sucess:true,
+      message:"Message updated Successfully",
+      problem:updatedProblem
+    });
+    
+  } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error: "Error While Creating Problem",
+      });
+  }
 }
 
 export const deleteProblem = async (req, res) => {
