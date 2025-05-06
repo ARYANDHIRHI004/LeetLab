e541f589f7fda393a8597435a400cb39ff24344d
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-05-06 09:56:39 +0530

    Respomsive welcome page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
     <>
         <div className='flex bg-black h-[100vh] items-center text-purple-50 justify-center '>
             <div className='text-center'>
-              <h1 className='text-7xl font-bold m-4'> Welcome To <span className='font-extrabold text-purple-600'>Neurocodium</span></h1>
-              <h2 className='text-center text-5xl font-semibold'>We are Coming Soon...</h2>
+              <h1 className='text-[4vw] font-bold m-4'> Welcome To <span className='font-extrabold text-purple-600'>Neurocodium</span></h1>
+              <h2 className='text-[2vw] text-center text-5xl font-semibold'>We are Coming Soon...</h2>
             </div>
         </div>
     </>
