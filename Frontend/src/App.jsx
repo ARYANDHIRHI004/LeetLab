80887c75539f0d329b795c5b2f6bad275d6ed6f9
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-05-06 10:52:50 +0530

    logo added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-
+import neurocodiumLogo from "./assets/neurocodiumLogo.png"
 function App() {
 
   return (
     <>
         <div className='flex bg-linear-to-r from-[#11002e] to-black h-[100vh] items-center text-purple-50 justify-center '>
-            <div className='text-center'>
+            <div className='text-center flex flex-col items-center'>
+            <img className='w-[20vh]' src={neurocodiumLogo} alt="hitesh sir" />
               <h1 className='text-5xl font-bold m-4 text-shadow-white'> Welcome To <span className='font-extrabold text-purple-600'>Neurocodium</span></h1>
               <h2 className='text-center text-2xl font-semibold'>We are Coming Soon...</h2>
             </div>
