97d758fc928c7339091644fe0e528fc758d2bc99
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-05-06 09:52:43 +0530

    New welcome page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,8 +4,12 @@ function App() {
 
   return (
     <>
-        <h1>Welcome To <span>Neurocodium</span></h1>
-        <h2>We are Coming Soon...</h2>
+        <div className='flex bg-black h-[100vh] items-center text-purple-50 justify-center '>
+            <div className='text-center'>
+              <h1 className='text-7xl font-bold m-4'> Welcome To <span className='font-extrabold text-purple-600'>Neurocodium</span></h1>
+              <h2 className='text-center text-5xl font-semibold'>We are Coming Soon...</h2>
+            </div>
+        </div>
     </>
   )
 }
