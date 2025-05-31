7b9a305d7d22a0e47690b5692672d344d32b3f33
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-05-31 12:26:32 +0530

    env configuration

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.BASEURL,
+    baseURL: import.meta.env.VITE_BASEURL,
     withCredentials: true,
 })
