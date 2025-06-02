0a45040339a39e09e996afca9b6713b5c0009e5b
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-03 04:51:38 +0530

    recreating pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,53 +1,13 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
-import HomePage from './pages/HomePage'
-import { Toaster } from 'react-hot-toast'
-import { useAuthStore } from './Store/useAuthStore'
-import { CloudCog, Loader } from 'lucide-react'
-import Layout from './components/Layout'
-import LogoutOutlet from './components/LogoutOutlet'
-import CreateProblemForm from './components/AddProblemForm' 
-import ProblemPage from './pages/ProblemPage'
-import WelcomePage from './pages/WelcomePage'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
-const App = () => {
-
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
-
-  useEffect((params) => {
-    checkAuth()
-  },[checkAuth])
-
-  if(isCheckingAuth && !authUser){
-    return (
-      <div className='h-screen flex justify-center items-center'>
-        <Loader className='size-10 animate-spin' ></Loader>
-      </div>
-    )
-  }
-
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='flex flex-col items-center justify-start w-full'>
-        <Toaster />
-        <Routes>
-          <Route path='/home' element={<Layout />}>
-            <Route path='/home' element={ authUser? <HomePage />: <Navigate to={"/login"}/>} />   
-            <Route path='/home/add-problem' element={ authUser? <CreateProblemForm />: <Navigate to={"/login"}/>} />   
-          </Route>
-            <Route path='/problem/:id' element={ authUser? <ProblemPage />: <Navigate to={"/login"}/>} />   
-
-          
-          <Route path='/' element={<LogoutOutlet />}>
-            <Route path='/' element={ !authUser? <WelcomePage />: <Navigate to={"/home"}/>} />
-            <Route path='/login' element={ !authUser? <LoginPage />: <Navigate to={"/home"}/>} />
-            <Route path='/signup' element={!authUser?<SignUpPage />: <Navigate to={"/home"}/>} />
-          </Route>
-        </Routes>        
-      </div>
+      <h1>Aryan</h1>
     </>
   )
 }
