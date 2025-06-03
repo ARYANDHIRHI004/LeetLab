0a4edba729b21a428c3d4776fb13aaa156d23d2c
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-03 21:20:27 +0530

    feat: problem table created

--- Frontend/src/Store/useProblemStore.js
+++ Frontend/src/Store/useProblemStore.js
@@ -14,7 +14,6 @@ export const useProblemStore = create((set) => ({
         
         set({isProblemsLoading: true})
         const res = await axiosInstance.get("/problems/get-all-problems")
-        console.log(res);
         
         set({problems: res.data.problems})
         
