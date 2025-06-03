9928b73b7df18610c3ef995e2ed43b7a1027d5e8
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-03 08:27:44 +0530

    login and signup page dreated

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8080/api/v1",
+    baseURL: 'http://127.0.0.1:8080/api/v1',
     withCredentials: true,
+    headers: {
+		Accept: 'application/json',
+	},
 })
