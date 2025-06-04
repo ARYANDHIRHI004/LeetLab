6e5a85898e22d5d5eb4b3f483c00ad80bde841fc
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-04 11:48:50 +0530

    problem page ui updated

--- Frontend/src/pages/ProblemPage.jsx
+++ Frontend/src/pages/ProblemPage.jsx
@@ -51,42 +51,54 @@ const ProblemPage = () => {
   if(isProblemLoading){
   return (
     <div className="h-[100vh] flex justify-center items-center">
-        <Database color="#364153" size={"50px"} className="animate-bounce" />
+        <Database color="#3000cf" size={"50px"} className="animate-bounce" />
     </div>
   )}
 
 
   return (
-    <div className=" flex bg-black h-[100vh] justify-center gap-1.5 box w-[100%] p-2">
+    <div className=" flex bg-[#111142] h-[100vh] justify-center gap-1.5 box w-[100%] p-2">
       <div
         style={{ width: leftWidth }}
-        className={`bg-gray-700 rounded-xl p-4`}
+        className={`bg-[#05001C] rounded-xl p-4`}
       >
-        <div className="text-2xl font-black text-white flex gap-2">
+        <div className="text-2xl font-black text-[#8EC5FF] flex items-center gap-2">
             <Link to={"/"}>
-              <House  color="white" />
+              <House  color="#8EC5FF" size={22} />
             </Link>
-            {problem.title}
+            {problem?.title}
         </div>
       </div>
       <div
         onMouseDown={horizontalMouseDown}
-        className="bg-gray-800 w-2 rounded-2xl hover:cursor-e-resize"
-      ></div>
+        className="bg-[#201e91] w-2 rounded-2xl hover:cursor-e-resize hover:bg-blue-800 flex items-center justify-center"
+      >
+        <div className="bg-white w-full h-10 rounded-2xl flex flex-col items-center justify-between p-[4px]">
+          <div className="bg-black h-[5px] w-[5px] rounded-full" ></div>
+          <div className="bg-black h-[5px] w-[5px] rounded-full" ></div>
+          <div className="bg-black h-[5px] w-[5px] rounded-full" ></div>
+        </div>
+      </div>
       <div style={{ width: rightWidth }} className="flex flex-col gap-1.5">
         <div
           style={{ height: upHight }}
-          className="bg-gray-300 h-[60vh] rounded-xl p-4"
+          className="bg-[#05001C] h-[60vh] rounded-xl p-4"
         >
           Up
         </div>
         <div
           onMouseDown={verticalMouseDown}
-          className="bg-gray-300 h-1.5 rounded-2xl hover:cursor-s-resize"
-        ></div>
+          className="bg-blue-900 h-1.5 rounded-2xl hover:cursor-s-resize hover:bg-blue-800 flex items-center justify-center"
+        >
+          <div className="bg-white w-10 h-full rounded-2xl flex items-center justify-between p-[4px]">
+          <div className="bg-black h-[5px] w-[5px] rounded-full" ></div>
+          <div className="bg-black h-[5px] w-[5px] rounded-full" ></div>
+          <div className="bg-black h-[5px] w-[5px] rounded-full" ></div>
+        </div>
+        </div>
         <div
           style={{ height: bottomHight }}
-          className="bg-gray-300 h-[40vh] rounded-xl p-4"
+          className="bg-[#05001C] h-[40vh] rounded-xl p-4"
         >
           Bottom
         </div>
