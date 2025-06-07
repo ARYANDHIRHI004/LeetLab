f74b3ee606064a30d0998ce686e892636a853f9e
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-07 16:22:15 +0530

    admin dashboard updated

--- Frontend/src/components/CreateUserPlaylist.jsx
+++ Frontend/src/components/CreateUserPlaylist.jsx
@@ -1,42 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { usePlaylist } from "../Store/usePlaylist";
-import { Link } from "react-router-dom";
+import { data, Link } from "react-router-dom";
 import { useActions } from "../Store/useActions";
 import { useProblemStore } from "../Store/useProblemStore";
 import AddQuestionsToPlayListForm from "./AddQuestionsToPlayListForm";
 
 const CreateUserPlaylist = () => {
   const { handleSubmit, register } = useForm();
-  const { playlists, isPlaylistsLoading, createPlaylist, addProblemsInPlaylist, getAllPlaylist } = usePlaylist();
+  const {
+    playlists,
+    isPlaylistsLoading,
+    createPlaylist,
+    addProblemsInPlaylist,
+    getAllPlaylist,
+  } = usePlaylist();
   const { selectWhichQustion, setselectWhichQustion } = useActions();
 
-  const { getAllProblems, problems, isProblemsLoading, } = useProblemStore();
+  const { getAllProblems, problems, isProblemsLoading } = useProblemStore();
 
-
+  
   const submitPlaylistData = (data) => {
     createPlaylist(data);
+    console.log(data)
   };
 
+  useEffect(() => {
+    getAllProblems();
+  }, [getAllProblems]);
 
-  useEffect(
-    () => {
-      getAllProblems();
-    },
-    [getAllProblems]
-  );
-  
-    useEffect(() => {
-      getAllPlaylist();
-    }, [getAllPlaylist]);
+  useEffect(() => {
+    getAllPlaylist();
+  }, [getAllPlaylist]);
 
   return (
     <div className="bg-black h-[100vh] pt-20 text-white px-5">
       <div className="flex gap-5">
         <div>
-          <div className="bg-gray-800 h-[38vh] w-[40vw] rounded-[5px] p-5 mb-5">
+          <div className="bg-[#131F36] h-[90vh] w-[40vw] rounded-[5px] px-5 ">
             <form
-              className="bg-gray-900 p-5 flex flex-col gap-8 rounded-[5px]"
+              className="p-5 flex flex-col gap-7 rounded-[5px]"
               action={handleSubmit(submitPlaylistData)}
             >
               <div className="flex flex-col">
@@ -61,19 +64,75 @@ const CreateUserPlaylist = () => {
                   {...register("description")}
                 />
               </div>
-              <button type="submit">Create PlayList</button>
+              <div className="flex flex-col">
+                <label className="text-[15px] mb-2" htmlFor="">
+                  Start Date :
+                </label>
+                <input
+                  className="bg-white rounded-[5px] p-2 text-black"
+                  placeholder="Enter Description"
+                  type="Date"
+                  {...register("eventDate")}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-[15px] mb-2" htmlFor="">
+                  End Date :
+                </label>
+                <input
+                  className="bg-white rounded-[5px] p-2 text-black"
+                  placeholder="Enter Description"
+                  type="Date"
+                  {...register("endDate")}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-[15px] mb-2" htmlFor="">
+                  Start Time :
+                </label>
+                <input
+                  className="bg-white rounded-[5px] p-2 text-black"
+                  placeholder="Enter Description"
+                  type="Time"
+                  {...register("eventTime")}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-[15px] mb-2" htmlFor="">
+                  End Time :
+                </label>
+                <input
+                  className="bg-white rounded-[5px] p-2 text-black"
+                  placeholder="Enter Description"
+                  type="Time"
+                  {...register("endTime")}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-[15px] mb-2" htmlFor="">
+                  Mode :
+                </label>
+                <select
+                  className="bg-white rounded-[5px] p-2 text-black"
+                  placeholder="Enter Description"
+                  type="Time"
+                  defaultValue={"ONLINE"}
+                  {...register("mode")}
+                >
+                  <option value="ONLINE">ONLINE</option>
+                  <option value="OFFLINE">OFFLINE</option>
+                </select>
+              </div>
+              <button
+                className="bg-gray-700 p-3 rounded-[5px] text-[15px]"
+                type="submit"
+              >
+                Create PlayList
+              </button>
             </form>
           </div>
-
-          <div className="bg-gray-800 h-[50vh] w-[40vw] rounded-[5px] p-5 flex flex-col justify-center items-center">
-            
-            
-            <button className="bg-white text-black w-[80%] h-[35px] rounded-[5px] p-3 text-center mt-5">
-              Add Questions To Playlist
-            </button>
-          </div>
         </div>
-        <div className="bg-gray-800 h-[90vh] w-[60vw] rounded-[5px] p-5">
+        <div className="bg-[#0e182b] h-[90vh] w-[60vw] rounded-[5px] p-5">
           <AddQuestionsToPlayListForm />
         </div>
       </div>
--- Frontend/src/pages/OrganizationDashboardPage.jsx
+++ Frontend/src/pages/OrganizationDashboardPage.jsx
@@ -13,91 +13,110 @@ const OrganizationDashboard = () => {
   }, [getAllPlaylist, createPlaylist]);
 
   return (
-    <div className="bg-black h-[100vh] text-white pt-19 px-5">
+    <div className="bg-[#000] h-[100vh] text-white pt-19 px-5">
       <div className="flex gap-5">
-        <div className="bg-gray-900 h-[90vh] w-[21vw] rounded-2xl"></div>
-        <div className="h-[90vh] w-[80vw] rounded-2xl px-5 pb-5 flex flex-col gap-10">
-          <div className="flex gap-12 min-h-80">
-            <div className="w-[50%]">
+        <div className="bg-[#131f36] h-[50vh] w-[21vw] rounded-2xl p-5 ">
+          <div className="flex flex-col items-center">
+            <div className=" mb-2 h-60 w-60 bg-white text-black rounded-full"></div>
+            <h2 className="text-[20px] flex justify-between mb-2">
+              {authUser?.name.toUpperCase()}
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[12px] flex justify-between mb-0.5">
+              Email: <span>{authUser?.email}</span>
+            </h2>
+            <h2 className="text-[12px] flex justify-between mb-0.5">
+              Role: <span>{authUser?.role}</span>
+            </h2>
+          </div>
+        </div>
+        <div className="h-[50vh] w-[80vw] rounded-2xl px-5 pb-5 flex flex-col gap-10">
+          <div className="flex gap-12 ">
+            <div  className="w-[37vw]">
               <h3 className="text-[20px] mb-3">Previous Events</h3>
-              <div className="flex flex-col gap-2">
-                {playlists?.map((playlist, i) =>
-                  playlist.isActive !== true ? (
-                    <Link to={"/previous-event"}>
-                      <div>
-                        <p
-                          className={`text-[15px] ${
-                            i % 2 == 0 ? "bg-[#181818]" : "bg-[#0c0c0c]"
-                          } p-2 rounded-[3px]`}
-                        >
-                          {playlist.name}
-                        </p>
-                      </div>
-                    </Link>
-                  ) : (
-                    ""
-                  )
-                )}
+              <div className="h-[42vh] overflow-auto rounded-[5px]" >
+                <div className="flex flex-col gap-2 ">
+                  {playlists?.map((playlist, i) =>
+                    playlist.isActive !== true ? (
+                      <Link to={"/previous-event"}>
+                        <div>
+                          <p
+                            className={`text-[15px] ${
+                              i % 2 == 0 ? "bg-[#2b2b2b]" : "bg-[#1d1d1d]"
+                            } p-2 rounded-[3px]`}
+                          >
+                            {playlist.name}
+                          </p>
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )
+                  )}
+                </div>
               </div>
             </div>
-            <div className="w-[50%]">
+            <div className="w-[37vw]" >
               <h3 className="text-[20px] mb-3">Ongoing Events</h3>
-              <div className="flex flex-col gap-2">
-                {playlists?.map((playlist, i) =>
-                  playlist.isActive === true ? (
-                    <Link to={"/previous-event"}>
-                      <div>
-                        <p
-                          className={`text-[15px] ${
-                            i % 2 == 0 ? "bg-[#181818]" : "bg-[#0c0c0c]"
-                          } p-2 rounded-[3px]`}
-                        >
-                          {playlist.name}
-                        </p>
-                      </div>
-                    </Link>
-                  ) : (
-                    ""
-                  )
-                )}
+              <div className="h-[42vh] overflow-auto rounded-[5px]">
+                <div className="flex flex-col gap-2 overflow-scroll ">
+                  {playlists?.map((playlist, i) =>
+                    playlist.isActive === true ? (
+                      <Link to={"/previous-event"}>
+                        <div>
+                          <p
+                            className={`text-[15px] ${
+                              i % 2 == 0 ? "bg-[#2b2b2b]" : "bg-[#1d1d1d]"
+                            } p-2 rounded-[3px]`}
+                          >
+                            {playlist.name}
+                          </p>
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )
+                  )}
+                </div>
               </div>
             </div>
           </div>
-          <div>
-            <div className="flex gap-5">
-              <Link
-                className="w-[33%] bg-[#161616] px-5 pt-5 h-[30vh]  rounded-2xl"
-                to={"/create-event"}
-              >
-                <div>
-                  <h2 className="text-2xl">Create Events</h2>
-                </div>
-              </Link>
-              <Link
-                className="w-[33%] bg-[#161616] px-5 pt-5 h-[30vh]  rounded-2xl"
-                to={"/create-assignments"}
-              >
-                <div>
-                  <h2 className="text-2xl">Add Users To Events</h2>
-                </div>
-              </Link>
-
-              <div className="w-[33%] bg-[#161616] px-5 pt-5 h-[30vh] rounded-2xl flex flex-col gap-3">
-                <div className="bg-[#2c2c2c] p-2 rounded-2xl">
-                  <h2 className="text-[13px]">Create Questions</h2>
-                </div>
-                <Link to={"/all-created-questions"}>
-                    <div className="bg-[#2c2c2c] p-2 rounded-2xl">
-                  <h2 className="text-[13px]">Questions Created</h2>
-                </div>
-                </Link>
-                <Link to={"/all-previously-assigned-users"}>
-                  <div className="bg-[#2c2c2c] p-2 rounded-2xl">
-                    <h2 className="text-[13px]">Previously Added Users</h2>
-                  </div>
-                </Link>
-              </div>
+        </div>
+      </div>
+      <div className="mt-5">
+        <div className="flex gap-5">
+          <Link
+            className="w-[33%] bg-[#161616] px-5 pt-5 h-[35vh]  rounded-2xl"
+            to={"/create-event"}
+          >
+            <div>
+              <h2 className="text-2xl">Create Events</h2>
             </div>
+          </Link>
+          <Link
+            className="w-[33%] bg-[#161616] px-5 pt-5 h-[35vh]  rounded-2xl"
+            to={"/create-assignments"}
+          >
+            <div>
+              <h2 className="text-2xl">Add Users To Events</h2>
+            </div>
+          </Link>
+
+          <div className="w-[33%] bg-[#161616] px-5 pt-5 h-[35vh] rounded-2xl flex flex-col gap-3">
+            <div className="bg-[#2c2c2c] p-2 h-15 text-center rounded-2xl">
+              <h2 className="text-[13px]">Create Questions</h2>
+            </div>
+            <Link to={"/all-created-questions"}>
+              <div className="bg-[#2c2c2c] p-2 h-15 text-center rounded-2xl">
+                <h2 className="text-[13px]">Questions Created</h2>
+              </div>
+            </Link>
+            <Link to={"/all-previously-assigned-users"}>
+              <div className="bg-[#2c2c2c] p-2 h-15 text-center rounded-2xl">
+                <h2 className="text-[13px]">Previously Added Users</h2>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
