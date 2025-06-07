7bd45802b1ee7d4d0960bb72b6f1eff29c45c534
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-07 17:15:26 +0530

    Home page ui update

--- Frontend/src/components/CreateUserPlaylist.jsx
+++ Frontend/src/components/CreateUserPlaylist.jsx
@@ -5,6 +5,7 @@ import { data, Link } from "react-router-dom";
 import { useActions } from "../Store/useActions";
 import { useProblemStore } from "../Store/useProblemStore";
 import AddQuestionsToPlayListForm from "./AddQuestionsToPlayListForm";
+import { useAuthStore } from "../Store/useAuthStore";
 
 const CreateUserPlaylist = () => {
   const { handleSubmit, register } = useForm();
@@ -18,11 +19,11 @@ const CreateUserPlaylist = () => {
   const { selectWhichQustion, setselectWhichQustion } = useActions();
 
   const { getAllProblems, problems, isProblemsLoading } = useProblemStore();
+  const { authUser } = useAuthStore();
 
-  
   const submitPlaylistData = (data) => {
     createPlaylist(data);
-    console.log(data)
+    console.log(data);
   };
 
   useEffect(() => {
@@ -34,10 +35,10 @@ const CreateUserPlaylist = () => {
   }, [getAllPlaylist]);
 
   return (
-    <div className="bg-black h-[100vh] pt-20 text-white px-5">
+    <div className="bg-black h-screen pt-20 text-white px-5">
       <div className="flex gap-5">
-        <div>
-          <div className="bg-[#131F36] h-[90vh] w-[40vw] rounded-[5px] px-5 ">
+        <div className="h-[90vh]">
+          <div className="bg-[#131F36] h-[90vh] w-[40vw] rounded-[5px] px-5 overflow-scroll">
             <form
               className="p-5 flex flex-col gap-7 rounded-[5px]"
               action={handleSubmit(submitPlaylistData)}
@@ -64,65 +65,73 @@ const CreateUserPlaylist = () => {
                   {...register("description")}
                 />
               </div>
-              <div className="flex flex-col">
-                <label className="text-[15px] mb-2" htmlFor="">
-                  Start Date :
-                </label>
-                <input
-                  className="bg-white rounded-[5px] p-2 text-black"
-                  placeholder="Enter Description"
-                  type="Date"
-                  {...register("eventDate")}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="text-[15px] mb-2" htmlFor="">
-                  End Date :
-                </label>
-                <input
-                  className="bg-white rounded-[5px] p-2 text-black"
-                  placeholder="Enter Description"
-                  type="Date"
-                  {...register("endDate")}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="text-[15px] mb-2" htmlFor="">
-                  Start Time :
-                </label>
-                <input
-                  className="bg-white rounded-[5px] p-2 text-black"
-                  placeholder="Enter Description"
-                  type="Time"
-                  {...register("eventTime")}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="text-[15px] mb-2" htmlFor="">
-                  End Time :
-                </label>
-                <input
-                  className="bg-white rounded-[5px] p-2 text-black"
-                  placeholder="Enter Description"
-                  type="Time"
-                  {...register("endTime")}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="text-[15px] mb-2" htmlFor="">
-                  Mode :
-                </label>
-                <select
-                  className="bg-white rounded-[5px] p-2 text-black"
-                  placeholder="Enter Description"
-                  type="Time"
-                  defaultValue={"ONLINE"}
-                  {...register("mode")}
-                >
-                  <option value="ONLINE">ONLINE</option>
-                  <option value="OFFLINE">OFFLINE</option>
-                </select>
-              </div>
+
+              {authUser.role === "ORGANIZATION" ? (
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col">
+                    <label className="text-[15px] mb-2" htmlFor="">
+                      Start Date :
+                    </label>
+                    <input
+                      className="bg-white rounded-[5px] p-2 text-black"
+                      placeholder="Enter Description"
+                      type="Date"
+                      {...register("eventDate")}
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-[15px] mb-2" htmlFor="">
+                      End Date :
+                    </label>
+                    <input
+                      className="bg-white rounded-[5px] p-2 text-black"
+                      placeholder="Enter Description"
+                      type="Date"
+                      {...register("endDate")}
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-[15px] mb-2" htmlFor="">
+                      Start Time :
+                    </label>
+                    <input
+                      className="bg-white rounded-[5px] p-2 text-black"
+                      placeholder="Enter Description"
+                      type="Time"
+                      {...register("eventTime")}
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-[15px] mb-2" htmlFor="">
+                      End Time :
+                    </label>
+                    <input
+                      className="bg-white rounded-[5px] p-2 text-black"
+                      placeholder="Enter Description"
+                      type="Time"
+                      {...register("endTime")}
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-[15px] mb-2" htmlFor="">
+                      Mode :
+                    </label>
+                    <select
+                      className="bg-white rounded-[5px] p-2 text-black"
+                      placeholder="Enter Description"
+                      type="Time"
+                      defaultValue={"ONLINE"}
+                      {...register("mode")}
+                    >
+                      <option value="ONLINE">ONLINE</option>
+                      <option value="OFFLINE">OFFLINE</option>
+                    </select>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+
               <button
                 className="bg-gray-700 p-3 rounded-[5px] text-[15px]"
                 type="submit"
