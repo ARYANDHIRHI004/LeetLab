8d8efcc1a9eb81acd7f8c932e77c5c76cd6ccd6f
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-08 15:44:17 +0530

    ui updated

--- Frontend/src/components/BlurBlob.jsx
+++ Frontend/src/components/BlurBlob.jsx
@@ -13,7 +13,7 @@ const BlurBlob = ({position, size}) => {
         transform:'translate(-50%, -50%)'
     }}
     >
-        <div className='w-full h-full bg-blue-800 rounded-full opacity-20 blur-3xl animate-pulse text-white'>
+        <div className='w-full h-full bg-blue-800 rounded-full opacity-20 blur-3xl animate-pulse text-white max-sm:hidden'>
         </div>
 
     </div>
